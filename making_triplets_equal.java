962357b369b541ba8d8f1166c159bea5b042ac16
Raul <akhuliraul@gmail.com>
2025-06-06 12:23:38 +0530

    add a file name making_triplets_equal

---
+++ making_triplets_equal.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class making_triplets_equal {
+    public static int getSteps(int a, int b, int c) {
+        int count = 0;
+        if(a == b || b == c){
+            if(a == b && b == c){
+                return 0;
+            }
+            else if(a == b){
+                while(a <= c){
+                    a++;
+                    c--;
+                    count++;
+                }
+            }
+            else return -1;
+        }
+        else return -1;
+        if(count > 1){
+            return count - 1;
+        }
+        else return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();  // number of test cases
+        int[][] testCases = new int[T][3];
+
+        System.out.println("make sure they are Ascending order: ");
+        // Read all test cases
+        for (int i = 0; i < T; i++) {
+            testCases[i][0] = sc.nextInt();
+            testCases[i][1] = sc.nextInt();
+            testCases[i][2] = sc.nextInt();
+        }
+
+        System.out.println("Output:");
+        for (int i = 0; i < T; i++) {
+            int a = testCases[i][0];
+            int b = testCases[i][1];
+            int c = testCases[i][2];
+
+            System.out.println(getSteps(a, b, c));
+        }
+
+        sc.close();
+    }
+}
