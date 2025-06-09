018dc78c8b8c5cc8da7f9b9c9e474a83a5d6a5c2
Raul <akhuliraul@gmail.com>
2025-06-09 21:29:32 +0530

    add a file to identify to find out max or min element without using array + ternary operator.

---
+++ max_min_element_without_array.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class max_min_element_without_array {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the no of element: ");
+        int n = sc.nextInt();
+        System.out.println("enter the elements: ");
+
+        int max = 0;
+        int min = n;
+        for(int i = 0; i < n; i++){
+            int m = sc.nextInt();
+            max = (max > m) ? max : m;
+            min = (min > m) ? m : min;
+        }
+        System.out.println(max);
+        System.out.println(min);
+    }
+}
+
+//using ternary operator.
