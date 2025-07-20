d7e7e5287f200db3d0bc1307b0c3e8ab367de175
Raul <akhuliraul@gmail.com>
2025-07-20 10:54:15 +0530

    add a file palindrome check for a number.

---
+++ palindrome_check_number.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class palindrome_check_number {
+    public static boolean isPalindrome(int num){
+        int number = num;
+        int reverse = 0;
+        while(num > 0){
+            int n = num %10;
+            num = num /10;
+            reverse = reverse *10 +n;
+        }
+        return number == reverse;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the number: ");
+        int n = sc.nextInt();
+        if(isPalindrome(n)){
+            System.out.println("\nIt is a palindrome number.");
+        }
+        else{
+            System.out.println("It is not a palindrome number.");
+        }
+    }
+}
